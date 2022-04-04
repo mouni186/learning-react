33b00acc620fbbecec846a3b0b75f977e3c0dcd7
mouni186 <mounikas1861999@gmail.com>
2022-04-04 23:09:30 +0530

    learned: UseState hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+  const [value, setvalue] = useState(0);
+  const [name,setName] = useState("");
+  const tutor = "Mounika";
+  const [theRandomNumber,setTheRandomNumber] = useState(0);
+  const randomNumberGenerator  = () => {
+   const number =  Math.random();
+   setTheRandomNumber(number);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+      <h1>hello world</h1>
+    <p>counter value:{value}</p>
+    <p>{[name,theRandomNumber]}</p>
+    <button onClick = {  randomNumberGenerator}>show my randomNumber</button>
+    <button onClick = { () => setName(tutor)}>show my name</button>
+    <button onClick = {() =>setvalue(value+1) }>+</button>
+    <button onClick = {() =>setvalue(value-1) }>-</button>
+   </div>
   );
 }
 
